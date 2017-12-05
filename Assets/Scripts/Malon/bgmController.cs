2804b05e6e6a8b2f7c57a388be25925ac9dc04b7
Michael Hung <michaelhung@shaw.ca>
2017-12-05 00:04:03 -0700

    implemented audio crossfade

--- Assets/Scripts/Malon/bgmController.cs
+++ Assets/Scripts/Malon/bgmController.cs
@@ -2,15 +2,131 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class bgmController : MonoBehaviour {
+public class BgmController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private int current;
+    private bool switching;
+    private int dest;
+    private const int MIN = 0;
+    private const int HALF = 1;
+    private const int MAX = 2;
+
+    public float gainFactor;
+
+    public AudioSource min;
+    public AudioSource half;
+    public AudioSource max;
+
+    void Start()
+    {
+        switching = false;
+        current = MIN;
+        dest = -1;
+    }
+
+    void Update()
+    {
+        if (switching && dest >= MIN)
+        {
+            switch (dest)
+            {
+                case MIN:
+                    if (current == MAX)
+                    {
+                        min.volume += gainFactor;
+                        max.volume -= gainFactor;
+                    } else
+                    {
+                        min.volume += gainFactor;
+                        half.volume -= gainFactor;
+                    }
+
+                    break;
+                case HALF:
+                    if (current == MAX)
+                    {
+                        half.volume += gainFactor;
+                        max.volume -= gainFactor;
+                    }
+                    else
+                    {
+                        half.volume += gainFactor;
+                        min.volume -= gainFactor;
+                    }
+                    break;
+                case MAX:
+                    if (current == MIN)
+                    {
+                        max.volume += gainFactor;
+                        min.volume -= gainFactor;
+                    }
+                    else
+                    {
+                        max.volume += gainFactor;
+                        half.volume -= gainFactor;
+                    }
+                    break;
+
+                default:
+                    Debug.Log("Something went wrong...");
+                    break;
+            }
+
+            if ((min.volume == 0 || min.volume == 1) && (half.volume == 0 || half.volume == 1) && (max.volume == 0 || max.volume == 1))
+            {
+                switching = false;
+                if (min.volume == 1)
+                    current = MIN;
+                else if (half.volume == 1)
+                    current = HALF;
+                else if (max.volume == 1)
+                    current = MAX;
+                dest = -1;
+            }
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (current != MIN)
+            {
+                //Debug.Log("Reached");
+                PlayMin();
+            }
+        }
+        else if (Input.GetButtonDown("Fire2"))
+        {
+            if (current != HALF)
+            {
+                //Debug.Log("Reached");
+                PlayHalf();
+            }
+        }
+
+        else if (Input.GetButtonDown("Fire3"))
+        {
+            if (current != MAX)
+            {
+                //Debug.Log("Reached");
+                PlayMax();
+            }
+        }
+    }
+
+    public void PlayMin()
+    {
+        switching = true;
+        dest = MIN;
+    }
+
+    public void PlayHalf()
+    {
+        switching = true;
+        dest = HALF;
+    }
+
+    public void PlayMax()
+    {
+        switching = true;
+        dest = MAX;
+    }
 }
